3a0ecd02061d1bdc6578005d4dc2707eb9397006
Sunny  Rai <su30rai213@gmail.com>
2026-06-29 14:09:51 +0530

    Implement website opening and song playing commands
    Added functionality to open websites and play songs based on voice commands. use of Dict.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import pyttsx3
 import time
 import musiclibrary
+import webbrowser
+
 from openai import OpenAI
 recognizer=sr.Recognizer()
 from dotenv import load_dotenv
@@ -38,34 +40,42 @@ def ask_ai(question):
     )
 
     return response.choices[0].message.content
-
+#if you want to add more websites , add here inside the website (in dict. format )
+websites = {
+    "google": "https://google.com",
+    "facebook": "https://facebook.com",
+    "linkedin": "https://linkedin.com",
+    "youtube": "https://youtube.com",
+    "whatsapp": "https://web.whatsapp.com",
+}
+    
 
 def processcommand(c):
-    if "open google" in c.lower():
-        speak("Opening google")
-        webbrowser.open("https://google.com")
+    command = c.lower().strip()
+    
+    if command.startwith("open");
+    website = command.replace( "open", "",1).strip()
+    
+        if website in websites:
+            speak(f"Opening {website}")
+            webbrowser.open(websites[website])
+        else:
+            speak("Sorry, I don't know that website.")
 
-    elif "open facebook" in c.lower():
-        speak("Opening facebook")
-        webbrowser.open("https://facebook.com")
-    elif "open linkedin" in c.lower():
-        speak("Opening linkedin")
-        webbrowser.open("https://linkedin.com")
-    elif "open youtube" in c.lower():
-        speak("Opening youtube")
-        webbrowser.open("https://youtube.com")
-    elif "open watsaap" in c.lower():
-        speak("Opening watsaap")
-        webbrowser.open("https://web.whatsapp.com")
-    elif c.lower().startswith("play"):
-        speak("playing the song")
-        song = c.lower().replace("play", "", 1).strip()
-        link=musiclibrary.music[song]
-        webbrowser.open(link)
-    elif "news" in c.lower():
+    
+    elif command.startswith("play"):
+        song = command.replace("play", "", 1).strip()
+
+        if song in musiclibrary.music:
+            speak(f"Playing {song}")
+            webbrowser.open(musiclibrary.music[song])
+        else:
+            speak("Sorry, I couldn't find that song.")
+
+
+    elif "news" in command:
         speak("News for today")
         webbrowser.open("https://www.thehindu.com/")
-    
     else:
         answer = ask_ai(c)
         print(answer)
@@ -95,6 +105,7 @@ if __name__=="__main__":
                     audio = recognizer.listen(source)
                 command=recognizer.recognize_google(audio)
                 processcommand(command)
+
 
         except sr.WaitTimeoutError:
             print("No speech detected.")
